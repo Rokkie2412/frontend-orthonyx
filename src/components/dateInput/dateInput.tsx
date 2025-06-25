031f83c2bad0fb3337c615ae72418c052a86f980
Ervan Jovian <ervanjovian@gmail.com>
2025-06-26 00:10:53 +0700

    add universal styling for button and settign date input

--- src/components/dateInput/dateInput.tsx
+++ src/components/dateInput/dateInput.tsx
@@ -26,6 +26,11 @@ const DatePickerField = ({
         onChange={onChange}
         placeholderText={placeholder}
         dateFormat={dateFormat}
+        maxDate={new Date()}
+        scrollableYearDropdown
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
         className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-400 focus:outline-none text-black"
       />
       {errorText && (
